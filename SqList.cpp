e1cad963665b14cf1d8263c13419d1438e3c298d
chaze <17byhejunjie@gmail.com>
2020-03-28 12:08:56 +1300

    Create SqList.cpp
    Interesting to see the output with swap order Elemtype[]

---
+++ SqList.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+typedef int ElemType;
+const int MAXSIZE = 20;
+enum Status{
+    ERROR=0,
+    OK,
+};
+
+using namespace std;
+
+struct SqList
+{
+    ElemType data[MAXSIZE];
+    ElemType alter[MAXSIZE];
+    int Length;//current length
+};
+
+ostream& operator<<(ostream& os,const SqList& sql)
+{
+    for(auto i:sql.data)
+        os<<i<<" ";
+    os<<"\n";
+
+     for(auto i:sql.alter)
+        os<<i<<" ";
+    os<<"\n";
+    return os;
+}
+
+//insert data at position
+Status insert(SqList& d, int pos, int val)
+{
+    //check position range
+    if(pos>MAXSIZE || pos<0 || pos >d.Length)
+        return Status::ERROR;
+    //shift all data if pos is not at end
+    if(pos<=d.Length)
+    {
+        for(int i=d.Length;i>=pos;--i)
+        {
+            d.data[i+1] = d.data[i];
+        }
+    }
+    //insert the val at pos
+    d.data[pos] = val;
+    return Status::OK;
+}
+
+int main()
+{
+    SqList s{1,2,3,4,5};
+    cout<<s;
+}
